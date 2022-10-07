4845e853c9e6e2cbac1fecdd9b2a339c37ff8091
ruben <room3622@gmail.com>
2022-10-07 08:45:55 +0100

    Update
    Signed-off-by: ruben <room3622@gmail.com>

--- app/Http/Controllers/ModulesController.php
+++ app/Http/Controllers/ModulesController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\RenderController;
 use http\Client\Curl\User;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Auth;
@@ -119,7 +120,7 @@ class ModulesController extends Controller
 
 
                 //return view("game");
-                //return view("login");
+                return view("login");
                 //return view("data");
                 //return view("Carater");
             }
@@ -227,7 +228,7 @@ class ModulesController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function index()
     {
@@ -237,7 +238,7 @@ class ModulesController extends Controller
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function create()
     {
@@ -247,8 +248,8 @@ class ModulesController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @return Response
      */
     public function store(Request $request)
     {
@@ -259,7 +260,7 @@ class ModulesController extends Controller
      * Display the specified resource.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function show($id)
     {
@@ -270,7 +271,7 @@ class ModulesController extends Controller
      * Show the form for editing the specified resource.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function edit($id)
     {
@@ -280,9 +281,9 @@ class ModulesController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param Request $request
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function update(Request $request, $id)
     {
@@ -293,7 +294,7 @@ class ModulesController extends Controller
      * Remove the specified resource from storage.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function destroy($id)
     {
